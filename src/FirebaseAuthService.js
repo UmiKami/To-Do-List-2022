9396545adbd2c653dd5339ba5eb1b5c095cfce96
umikami <ggoku02@gmail.com>
2022-10-09 08:35:11 -0400

    Firebase services | login / sign up / logout

---
+++ src/FirebaseAuthService.js
@@ -0,0 +1,48 @@
+import firebase from "./FirebaseConfig"
+
+const auth = firebase.auth()
+
+const registerUser = (email, password, username) => {
+    auth.createUserWithEmailAndPassword(email, password)
+    .then(res=>{
+        // after it creates the account, it updates the user with the username input
+        return res.user.updateProfile({
+            displayName: username
+        })
+    })
+}
+
+const loginUser = (email, password) => {
+    return auth.signInWithEmailAndPassword(email, password)
+}
+
+const logoutUser = () => {
+    return auth.signOut();
+}
+
+const sendPasswordResetEmail = (email) => {
+    return auth.sendPasswordResetEmail(email)
+}
+
+const loginWithGoogle = () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+
+    return auth.signInWithPopup(provider)
+}
+
+const subscribeToAuthChanges = (handleAuthChange) => {
+    auth.onAuthStateChanged(user => {
+        handleAuthChange(user)
+    })
+}
+
+const FirebaseAuthService = {
+    registerUser,
+    loginUser,
+    logoutUser,
+    sendPasswordResetEmail,
+    loginWithGoogle,
+    subscribeToAuthChanges,
+}
+
+export default FirebaseAuthService
